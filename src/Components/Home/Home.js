4e04212e05bb1a97830e896f781e3e39837a95cd
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-04-25 10:11:52 -0400

    Added basic content to Home component.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { css } from 'emotion';
 import Nav from '../../Components/Nav/Nav';
 import bgImage from '../../images/homeBG.jpg';
+import bgImage2 from '../../images/dragon-battle.jpg';
+import bgImage3 from '../../images/hero-sec4.jpg';
 
 export default class Home extends Component {
     render() {
@@ -15,11 +17,21 @@ export default class Home extends Component {
                     </div>
 
                     <div className={descriptionSection}>
-                        Scroll
+                        <p>An ancient and mysterious evil arises, forcing the Heroes of Swiftfell to once again defend the realm of Sundirra...</p>
+                        <p>... the Aeon Crystal, an ethereal relic beset on the side of light, has fallen into darkness...</p>
+                        <p>... a world now engulfed in despair cries out for a savior... will you answer?</p>
                     </div>
 
-                    <div className={test}>
-                        TEST
+                    <div className={dragonSection}>
+                        {/* BLANK */}
+                    </div>
+
+                    <div className={cardSection}>
+
+                    </div>
+
+                    <div className={battleSection}>
+
                     </div>
                 </div>
             </>
@@ -46,12 +58,42 @@ const titleSection = css`
     background-size: cover;
 `
 const descriptionSection = css`
-    height: 50vh;
+    height: 75vh;
     width: 100vw;
-    background-color: red;
+    background-position: center;
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.6em;
+    font-family: fantasy;
 `
-const test = css`
+const dragonSection = css`
     height: 93vh;
     width: 100vw;
-    background-color: gray;
+    background-image: url('${bgImage2}');
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+`
+const cardSection = css`
+    height: 75vh;
+    width: 100vw;
+`
+const battleSection = css`
+    height: 93vh;
+    width: 100vw;
+    background-image: url('${bgImage3}');
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+`
+const signUpSection = css`
+    height: 93vh;
+    width: 100vw;
+    background-color: rgba(0, 0, 0, 0);
 `
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import { css } from 'emotion';
+import bgImage from '../../images/title-bg.jpg';
+import titleText from '../../images/logo-text.png';
 
 export default class Nav extends Component {
     render() {
         return (
             <div className={main}>
-                NAV BAR
+                <div className={leftSection}>
+
+                </div>
+
+                <div className={centerSection}>
+                    <img src={titleText} className={titleTextStyle} alt="TFS Logo Text" />
+                </div>
+
+                <div className={rightSection}>
+
+                </div>
             </div>
         )
     }
@@ -14,4 +26,27 @@ export default class Nav extends Component {
 const main = css`
     height: 7vh;
     width: 100vw;
+    background-image: url('${bgImage}');
+    background-size: 100% 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-templates-rows: 100%;
+`
+const leftSection = css`
+    grid-area: 1 / 1 / span 1 / span 1;
+    display: flex;
+`
+const centerSection = css`
+    grid-area: 1 / 2 / span 1 / span 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const rightSection = css`
+    grid-area: 1 / 3 / span 1 / span 1;
+    display: flex;
+`
+const titleTextStyle = css`
+    height: 80%;
+    width: 80%;
 `
