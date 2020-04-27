06b860ce5ecf7dec17cda4c646088e3693e62e71
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-04-27 12:32:36 -0400

    Updated Axios file for AWS deployment

--- src/Components/lib/Axios/Axios.js
+++ src/Components/lib/Axios/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '',
+    baseURL: process.env.NODE_ENV === 'development' ? '/api' : '',
     timeout: 10000,
     headers: {
         'Access-Control-Allow-Origin': '*'
