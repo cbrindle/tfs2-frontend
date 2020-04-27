b88f42008258637064034846d0c57431f17f3138
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-04-27 13:36:16 -0400

    Created layout framework for About component.

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { css } from 'emotion';
 import Nav from '../Nav/Nav';
+import bgImage from '../../images/parchment.jpg';
 
 class About extends Component {
 
@@ -16,7 +17,9 @@ class About extends Component {
                 <Nav goHome={this.homeRedirect} />
 
                 <div className={main}>
-                    About
+                    <div className={contentMain}>
+
+                    </div>
                 </div>
             </>
         )
@@ -34,4 +37,15 @@ export default connect(mapStateToProps, {})(About)
 const main = css`
     height: 93vh;
     width: 100vw;
+    background-image: url('${bgImage}');
+    background-size: 100% 100%;
+    display: grid;
+    grid-template-columns: 10% 55% 35%;
+    grid-template-rows: 5% 90% 5%;
+`
+const contentMain = css`
+    grid-area: 2 / 2 / span 1 / span 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
